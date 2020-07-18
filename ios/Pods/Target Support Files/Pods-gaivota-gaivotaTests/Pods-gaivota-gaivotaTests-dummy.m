f1f86cce64948f230dce2e02f30aa521ff70faac
thomaz <>
2020-07-17 22:17:17 -0300

    criação do projeto

---
+++ ios/Pods/Target Support Files/Pods-gaivota-gaivotaTests/Pods-gaivota-gaivotaTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_gaivota_gaivotaTests : NSObject
+@end
+@implementation PodsDummy_Pods_gaivota_gaivotaTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-gaivota-tvOS/Pods-gaivota-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_gaivota_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_gaivota_tvOS
+@end
